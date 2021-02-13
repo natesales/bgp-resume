fed80438664a9a7ffe9074afcbdea2fb9c9a22d4
Nate Sales <nate@natesales.net>
2021-02-12 17:55:07 -0800

    add argument parsing

--- main.go
+++ main.go
@@ -10,54 +10,64 @@ import (
 	log "github.com/sirupsen/logrus"
 	"os"
 	"os/signal"
+	"strconv"
+	"strings"
 )
 
 var (
 	resumeFile      = flag.String("resume", "", "path to resume file")
-	localAsn        = flag.Uint("local-asn", 0, "local ASN")
+	asn             = flag.Uint("asn", 0, "ASN")
 	localAddress    = flag.String("local-addr", "", "local address to bind to")
-	localPort       = flag.Uint("local-port", 179, "local BGP listen port")
-	upstreamAsn     = flag.Uint("upstream-asn", 0, "upstream's ASN")
 	upstreamAddress = flag.String("upstream-addr", "", "upstream's peering address")
 	multihop        = flag.Bool("multihop", false, "enable BGP multihop")
+	routerId        = flag.String("router-id", "", "BGP router id")
+	prefix          = flag.String("prefix", "", "IPv6 prefix to announce")
 )
 
 func main() {
 	flag.Parse()
 
 	// Validate flags
-	if *resumeFile == "" || *localAsn == 0 || *localAddress == "" || *upstreamAsn == 0 || *upstreamAddress == "" {
+	if *resumeFile == "" || *asn == 0 || *localAddress == "" || *upstreamAddress == "" || *routerId == "" || *prefix == "" {
 		flag.Usage()
 		os.Exit(1)
 	}
 
-	log.SetLevel(log.DebugLevel)
+	// Parse prefix into network and mask
+	prefixParts := strings.Split(*prefix, "/")
+	prefixNetwork := prefixParts[0]
+	prefixMask, err := strconv.Atoi(prefixParts[1])
+	if err != nil {
+		log.Fatalf("unable to parse prefix mask: %v", err)
+	}
 
-	// Create GoBGP server
+	// TODO: add startup info about what's going on
+
+	log.SetLevel(log.DebugLevel)
 	s := gobgp.NewBgpServer()
 	go s.Serve()
 
-	// Setup BGP listener
+	// global configuration
 	if err := s.StartBgp(context.Background(), &api.StartBgpRequest{
 		Global: &api.Global{
-			As:         uint32(*localAsn),
-			RouterId:   *localAddress,
-			ListenPort: int32(*localPort),
+			As:         uint32(*asn),
+			RouterId:   *routerId,
+			ListenPort: 179,
 		},
 	}); err != nil {
 		log.Fatal(err)
 	}
 
-	// Monitor peer state change
+	// monitor the change of the peer state
 	if err := s.MonitorPeer(context.Background(), &api.MonitorPeerRequest{}, func(p *api.Peer) { log.Info(p) }); err != nil {
 		log.Fatal(err)
 	}
 
-	// Configure the upstream BGP session
+	// neighbor configuration
 	n := &api.Peer{
 		Conf: &api.PeerConf{
 			NeighborAddress: *upstreamAddress,
-			PeerAs:          uint32(*upstreamAsn),
+			PeerAs:          uint32(*asn),
 		},
 	}
 
@@ -67,45 +77,43 @@ func main() {
 		log.Fatal(err)
 	}
 
-	// Add route
-	a1, _ := ptypes.MarshalAny(&api.OriginAttribute{
-		Origin: uint32(*localAsn),
-	})
-
 	v6Family := &api.Family{
 		Afi:  api.Family_AFI_IP6,
 		Safi: api.Family_SAFI_UNICAST,
 	}
 
-	// add v6 route
+	// add routes
 	nlri, _ := ptypes.MarshalAny(&api.IPAddressPrefix{
-		PrefixLen: 64,
-		Prefix:    "2001:db8:1::",
+		Prefix:    prefixNetwork,
+		PrefixLen: uint32(prefixMask),
 	})
+
+	a1, _ := ptypes.MarshalAny(&api.OriginAttribute{
+		Origin: 0,
+	})
+
 	v6Attrs, _ := ptypes.MarshalAny(&api.MpReachNLRIAttribute{
 		Family:   v6Family,
-		NextHops: []string{"2001:db8::1"},
+		NextHops: []string{*localAddress},
 		Nlris:    []*any.Any{nlri},
 	})
 
-	c, _ := ptypes.MarshalAny(&api.CommunitiesAttribute{
-		Communities: []uint32{100, 200},
+	communities, _ := ptypes.MarshalAny(&api.CommunitiesAttribute{
+		Communities: []uint32{100, 200, 300},
 	})
 
-	_, err := s.AddPath(context.Background(), &api.AddPathRequest{
+	pathAttrs := []*any.Any{a1, v6Attrs, communities}
+
+	_, err = s.AddPath(context.Background(), &api.AddPathRequest{
 		Path: &api.Path{
-			Family: v6Family,
+			Family: &api.Family{Afi: api.Family_AFI_IP6, Safi: api.Family_SAFI_UNICAST},
 			Nlri:   nlri,
-			Pattrs: []*any.Any{a1, v6Attrs, c},
+			Pattrs: pathAttrs,
 		},
 	})
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	s.ListPath(context.Background(), &api.ListPathRequest{Family: v6Family}, func(p *api.Destination) {
-		log.Info(p)
-	})
 
 	// Wait until interrupt
 	interruptChannel := make(chan os.Signal, 1)
